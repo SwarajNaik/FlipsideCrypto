376abd144ab764d5183633662cf2c01a6254af2c
Swaraj Naik <naik.swaraj2007@gmail.com>
2023-06-11 14:25:00 +0530

    Frame for any data is ready

--- src/data_fetcthing.py
+++ src/data_fetcthing.py
@@ -1,19 +1,41 @@
 from flipside import Flipside
-from sequels import pools
+# from sequels import pools
+import pandas as pd
 
-
-flipside = Flipside("aacbd3da-5f83-4039-a08f-92ccd15fa9f1", "https://api-v2.flipsidecrypto.xyz")
-
-
-sql = """
-SELECT 
-  date_trunc('hour', block_timestamp) as hour,
-  count(distinct tx_hash) as tx_count
-FROM ethereum.core.fact_transactions 
-WHERE block_timestamp >= GETDATE() - interval'7 days'
-GROUP BY 1
+pools = """
+SELECT * FROM ethereum.uniswapv3.ez_pools 
+LIMIT 100
 """
 
+def execute_query(sql):
+  flipside = Flipside("aacbd3da-5f83-4039-a08f-92ccd15fa9f1", "https://api-v2.flipsidecrypto.xyz")
+  query_result_set = flipside.query(sql)
+  return query_result_set
 
-query_result_set = flipside.query(sql)
-print(query_result_set)
+# """pip install flipside-sdk"""
+# from flipside import Flipside
+
+# """Initialize Flipside with your API Key / API Url"""
+# flipside = Flipside("e3147454-8107-4d64-b673-b93b9c8e55fc", "https://api-v2.flipsidecrypto.xyz")
+# sql = """SELECT * FROM ethereum.uniswapv3.ez_swaps LIMIT 100"""
+# """Run the query against Flipside's query engine and await the results"""
+# query_result_set = flipside.query(sql)
+# print(query_result_set)
+
+def clean_data(data):
+  result = {}
+  
+  for column in data.columns:
+    result[column] = []
+  
+  for record in data.records:
+    for column in data.columns:
+      result[column].append(record[column])
+  
+  return pd.DataFrame(result)
+
+if __name__ == "__main__":
+  
+  sql = execute_query(pools)
+  
+  df = clean_data(data=sql)
--- src/sequels.py
+++ src/sequels.py
@@ -1,6 +1,14 @@
 pools = """
-
 SELECT * FROM ethereum.uniswapv3.ez_pools 
-LIMIT 9
+LIMIT 100
+"""
 
+
+sql = """
+SELECT 
+  date_trunc('hour', block_timestamp) as hour,
+  count(distinct tx_hash) as tx_count
+FROM ethereum.core.fact_transactions 
+WHERE block_timestamp >= GETDATE() - interval'7 days'
+GROUP BY 1
 """
